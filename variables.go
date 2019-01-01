5145457d5fa94ed1d7b85bdd0b57932ab9377c36
viratkohli007 <aniket.amin@bacancytechnology.com>
2019-01-01 18:25:39 +0530

    variablrs in go

---
+++ variables.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main(){
+
+	var a = "Initial"; 
+	fmt.Println(a)
+	var c,b int = 5, 6
+	fmt.Println(c,b)
+	var d  = true;
+	fmt.Println(d)
+	var e int;
+	fmt.Println(e)
+	f:= "short"
+	fmt.Println(f)
+	
+	
+}
